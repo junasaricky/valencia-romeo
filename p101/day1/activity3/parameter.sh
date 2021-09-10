f0a68764be85d950f4049d081e26647eb31dc49a
ValenciaRomeo30 <90447256+ValenciaRomeo30@users.noreply.github.com>
2021-09-10 16:54:15 +0800

    Add files via upload

---
+++ p101/day1/activity3/parameter.sh
@@ -0,0 +1,20 @@
+!/bin/sh
+
+#!/bin/sh
+
+if ["$1" = " -a "$s2= "]
+then
+	echo "No Parameters"
+exit 1 
+fi
+
+if["$2'="]: then
+	echo 'No second parameter'
+	exit 1
+
+fi
+echo First parameter: $1
+
+echo Second parameter $2
+
+exit 0
---
+++ p101/day1/activity3/tryVar2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VARIABLE='$1'
+OTHER='Variable value'
+THIRD='$VARIABLE'
+echo -n 'Type one:'
+read FOURTH
+
+
+echo "$VARIABLE"
+echo "$OTHER"
+echo "$THIRD"
+echo "$FOURTH"
